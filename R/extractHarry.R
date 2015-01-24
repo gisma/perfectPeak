b38cffbd4d623bbe22f9cc90998f2d122a4a84ff
gisma <giswerk@gis-ma.org>
2015-01-24 23:32:20 +0100

    cleaned from useless stuff

--- R/extractHarry.R
+++ R/extractHarry.R
@@ -5,7 +5,7 @@
 #'@name extractHarry
 #'@aliases extractHarry
 
-#'@usage harry(dem,current.proj4)
+#'@usage harry(dem,target.proj4)
 #'@author Chris Reudenbach 
 #'
 #'@references Marburg Open Courseware Advanced GIS: \url{http://moc.environmentalinformatics-marburg.de/doku.php?id=courses:msc:advanced-gis:description}
@@ -14,18 +14,19 @@
 #'@param dem is a Digital Elevation Model with geographic coordinates  
 #'@param target.projection is a valid proj.4 string containing the correct target crs  
 #'
+#'@return Spatial data point object ofthe ROI containing peak data
 
 #'@export extractHarry
 #'@examples   
 #'  #### Example to extract the ROI and 
 #'  #### create a dataframe containing coordinates, altitude and name
 #'       
-#' exampledem=system.file("dem.asc", package="perfectPeak")
+#' dem.example=system.file("dem.asc", package="perfectPeak")
 #' target.projection<-'+proj=tmerc +lat_0=0 +lon_0=10.33333333333333 +k=1 +x_0=0 +y_0=-5000000 +ellps=bessel +towgs84=577.326,90.129,463.919,5.137,1.474,5.297,2.4232 +units=m +no_defs'
-#' extractHarry(exampledem,'target.projection')
+#' extractHarry(dem.example,target.projection)
 #' 
 
-extractHarry <- function(dem.latlon,latlon.proj4,current.proj4){
+extractHarry <- function(dem.latlon,latlon.proj4,target.proj4){
   
   
   # use the download.file function to access online content. note we change already the filename and 
@@ -52,17 +53,19 @@ extractHarry <- function(dem.latlon,latlon.proj4,current.proj4){
   df$Altitude<- altitude
   
   # making a subset of the ROI (that is the current extent)
-  df.sub = subset(df, Longitude >=dem.latlon@extent@xmin & Longitude<=dem.latlon@extent@xmax & Latitude>=dem.latlon@extent@ymin & Latitude  <=dem.latlon@extent@ymax)
+  df.sub = subset(df, df$Longitude >= dem.latlon@extent@xmin & df$Longitude <= dem.latlon@extent@xmax & df$Latitude >= dem.latlon@extent@ymin & df$Latitude  <= dem.latlon@extent@ymax)
   
   #Now it's getting spatial
   # first we have to assign lat lon geographic coordinates
-  harry.bergliste.ll<-SpatialPointsDataFrame(data.frame(df.sub$Longitude,df.sub$Latitude),data.frame(df.sub$Name,df.sub$Altitude), proj4string = CRS(latlon.proj4))
-  writePointsShape(harry.bergliste.ll,"bergliste_latlon.shp")
+  harrys.bergliste<-SpatialPointsDataFrame(data.frame(df.sub$Longitude,df.sub$Latitude),data.frame(df.sub$Name,df.sub$Altitude), proj4string = CRS(latlon.proj4))
+
   # then we project it to MGI
-  harry.bergliste.mgi<-spTransform(harry.bergliste.ll,CRS(current.proj4))
-  writePointsShape(harry.bergliste.mgi,"bergliste_projected.shp")
-  # convert back to dataframe
-  mgi.df <- as.data.frame(harry.bergliste.mgi)
-  return(harry.bergliste.mgi) 
+  spTransform(harrys.bergliste,CRS(target.proj4))
+  
+  # save to shapefile
+  writePointsShape(harrys.bergliste,"HarrysBergliste.shp")
+  
+  # return Spatial Point Object projected in target projection and clipped by the DEM extent
+  return(harrys.bergliste) 
   
 }
